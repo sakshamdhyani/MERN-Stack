7dcdb939fd4e82978c8e963900ed4f10450e4bfc
Saksham Dhyani <106466420+sakshamdhyani@users.noreply.github.com>
2023-08-30 14:45:12 +0530

    Started Frontend
    - will start implementing Redux

---
+++ E-commerce/frontend/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import './App.css';
+import Header from './component/layout/Header/Header.js';
+import { BrowserRouter as Router , Route , Routes} from 'react-router-dom';
+import WebFont from "webfontloader";
+import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js"
+
+
+function App() {
+
+  // Rendering Font Families
+  useEffect(() => {
+
+    WebFont.load({
+      google:{
+        families:["Roboto" , "Droid Sans" , "Chilanka"],
+      },
+      
+    });
+  
+  },[]);
+
+
+  return (
+
+    <Router>
+      <Header/>
+
+      <Routes>
+        <Route exact path='/' element = {<Home/>} />
+      </Routes>
+      
+
+      <Footer/>
+    </Router>
+
+  );
+}
+
+export default App;
