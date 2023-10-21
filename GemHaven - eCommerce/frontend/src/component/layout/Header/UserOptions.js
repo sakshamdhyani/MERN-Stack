3d417c3c6a6cd0bdf104bfaaa6ecd8bcb52277f3
Saksham Dhyani <106466420+sakshamdhyani@users.noreply.github.com>
2023-10-21 19:58:00 +0530

    backed successfully connected with frontend

--- GemHaven - eCommerce/frontend/src/component/layout/Header/UserOptions.js
+++ GemHaven - eCommerce/frontend/src/component/layout/Header/UserOptions.js
@@ -80,7 +80,7 @@ const UserOptions = ({user}) => {
         
         icon = {<img 
             className='speedDialIcon'
-            src={user.avatar.url ? user.avatar.url: Profile}
+            
             alt='Profile'
         />}
         >
